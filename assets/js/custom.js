f118048749c85b50460d0a8cecde99da36eb0fe4
gvillar <guillevillarrodriguez@gmail.com>
2017-06-17 19:42:08 +0200

    actualizacion tras fallo dado

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,14 +7,11 @@
 		});
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 
-<<<<<<< HEAD
     var width = $(document).width();
     $('svg').attr('width', width);
     console.log(width)
 
 
-=======
->>>>>>> 06a8eba722b420cb7a89554ef81a0ff16aa9148c
     $('button[data-province]').on('click', function(e){
       e.preventDefault();
 
@@ -23,16 +20,9 @@
       $(this).addClass('active');
       var year = $(this).html();
 
-<<<<<<< HEAD
       var newUrl = "assets/img/sankey/" + province + "/sankey_" + province + "_" + year + ".png";
                     
       $('img[data-province="'+province+'"]').attr('src', newUrl)
-=======
-      var newUrl = "assets/img/sankey_svg/svg_" + province + "/" + province + "_" + year + ".svg";
-
-      $('object[data-province="'+province+'"]').attr('data', newUrl);
-
->>>>>>> 06a8eba722b420cb7a89554ef81a0ff16aa9148c
     });
 	});
 
