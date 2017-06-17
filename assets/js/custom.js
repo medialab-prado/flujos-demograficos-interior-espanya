3afa5ad2bf4da912237222bee3331169b8081f54
Fernando Blat <ferblape@gmail.com>
2017-06-17 13:21:44 +0200

    Integrado botonera

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,23 @@
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
+		});
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
 
-		
+    $('button[data-province]').on('click', function(e){
+      e.preventDefault();
+
+      var province = $(this).data('province');
+      $('button[data-province="'+province+'"]').removeClass('active');
+      $(this).addClass('active');
+      var year = $(this).html();
+
+      var newUrl = "/assets/img/sankey_svg/svg_" + province + "/" + province + "_" + year + ".svg";
+
+      $('object[data-province="'+province+'"]').attr('data', newUrl);
+
+    });
+	});
+
+
 })(jQuery);
