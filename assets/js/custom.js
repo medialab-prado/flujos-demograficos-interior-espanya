230729299579ce79f593c9d85e9f2eee611cd88d
gvillar <guillevillarrodriguez@gmail.com>
2017-06-17 19:29:05 +0200

    actualizacion previa a la doble seleccion por provincia y año

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,27 @@
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
+		});
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
 
-		
+    var width = $(document).width();
+    $('svg').attr('width', width);
+    console.log(width)
+
+
+    $('button[data-province]').on('click', function(e){
+      e.preventDefault();
+
+      var province = $(this).data('province');
+      $('button[data-province="'+province+'"]').removeClass('active');
+      $(this).addClass('active');
+      var year = $(this).html();
+
+      var newUrl = "assets/img/sankey/" + province + "/sankey_" + province + "_" + year + ".png";
+                    
+      $('img[data-province="'+province+'"]').attr('src', newUrl)
+    });
+	});
+
+
 })(jQuery);
